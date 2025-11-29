1222ae32b62ee5bfce4ad68b0a07a3a40c64c44c
Luan Santhiago <luansanthiago26@gmail.com>
2025-11-29 18:57:07 -0300

    Sobre estruturas condicionais

---
+++ 03-operadores-aritmericos.js
@@ -0,0 +1,39 @@
+// Operadores Aritméticos em JavaScript
+// Servem para realizar operações matemáticas básicas
+
+// O mais comuns são:
+// Adição (+)
+
+let x = Number(prompt("Digite um número: "));
+let y = Number(prompt("Digite outro número: "));
+
+let soma = x + y;
+
+alert('O resultado da soma entre ${x} e ${y} é: ${soma}');
+console.log(soma);
+
+// Subtração (-)
+let subtracao = x - y;
+console.log(subtracao);
+alert('O resultado da subtração entre ${x} e ${y} é: ${subtracao}');
+
+// Multiplicação (*)
+let multiplicacao = x * y;
+console.log(multiplicacao);
+alert('O resultado da multiplicação entre ${x} e ${y} é: ${multiplicacao}');
+
+// Divisão (/)
+let divisao = x / y;
+console.log(divisao);
+alert('O resultado da divisão entre ${x} e ${y} é: ${divisao}');
+
+// Exponenciação (**)
+let exponenciacao = x ** y;
+console.log(exponenciacao);
+alert('O resultado da exponenciação entre ${x} e ${y} é: ${exponenciacao}');
+
+// Resto da divisão (%)
+let resto = x % y;
+console.log(resto);
+alert('O resto da divisão entre ${x} e ${y} é: ${resto}');
+
